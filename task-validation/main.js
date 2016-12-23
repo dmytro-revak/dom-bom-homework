45401887f83499d7404c49886d3b24f04a635ad4
dmytro-revak <dmytro27@gmail.com>
2016-12-23 02:19:27 +0200

    complitely and the validation task

--- task-validation/main.js
+++ task-validation/main.js
@@ -1,4 +1,5 @@
 (function implementValidaton() {
+  // At the first we must creat a form
   createForm();
 
   // The next array has inside the future form elements with their attributes 
@@ -27,12 +28,14 @@
     }
   ];
 
+  //Get form which we need
   var $currentForm = document.querySelector('[name="login"]');
   
   createAndAddFormElements(formElementsDefaultList, $currentForm);
-  // validation($currentForm);
 
-// The next function get objects from default list and craete all form elements, add attribures which they need and set then in the form
+// Start functions describe
+
+  // The next function get objects from default list and craete all form elements, add attribures which they need and set then in the form
   function createAndAddFormElements(formElementsDefaultList, $currentForm) {
     for (var i = 0; i < formElementsDefaultList.length; i++) {
       var $currentElement = formElementsDefaultList[i];
@@ -49,156 +52,149 @@
     $form.name = 'login';
     $form.action = 'google.com';
     document.body.insertBefore( $form, document.querySelector('[type="text/javascript"]') );
- }
-
-
-
-
-
-
-// The next var has all elements which nedd to validate
- var $elementsForValidation = $currentForm.querySelectorAll('[data-validate]');
-
-
-//  The next function start validation for current form and for all elemenst insdie the form
-validation($currentForm, $elementsForValidation);
- // console.log($elementsForValidation);
-
-
-
-function getAllCorrectPlaceholders() {
-  
-}
-
- function validation($currentForm, $elementsForValidation) {
-   $currentForm.addEventListener('submit', function (e) {
-    e.preventDefault();
-    // console.log('on submit');
-   var isFormValid;
-debugger;
-
-    
-removeErrorBorder($elementsForValidation);
-
-// The nex function reset all red error borders from all fields 
-function removeErrorBorder($elementsForValidation) {
-  for (var i = 0; i < $elementsForValidation.length; i++) {
-    $elementsForValidation[i].classList.remove('errorField');
   }
-}
+
+// End functions describe
+
+// Star validation
+
+  // The next variable has all elements which nedd to validate
+  var $elementsForValidation = $currentForm.querySelectorAll('[data-validate]');
 
 
-    eachElementsValidation($elementsForValidation);
+  //  The next function, if you click 'submit', start validation for current form and for all elemenst insdie the form
+  validation($currentForm, $elementsForValidation);
 
-// The next function gets each field and applies validation for it. Also it has an  the object with all validation functions
-function eachElementsValidation($elementsForValidation) {
+  // The next function gets and saves correct placeholders each of element
+  var correctPlaceholders = [];
+  getAllCorrectPlaceholders($elementsForValidation);
 
-  function highlightErrorField(errorElemet) {
-    errorElemet.classList.add('errorField');
-    errorElemet.value = '';
-    errorElemet.setAttribute('placeholder', 'Error! Empty/incorrectly filled');
+  function getAllCorrectPlaceholders($elementsForValidation) {
+    for ( var i = 0; i < $elementsForValidation.length; i++) {
+      var currentCorrectPlaceholder = $elementsForValidation[i].getAttribute('placeholder');
+      correctPlaceholders.push(currentCorrectPlaceholder);
+    }
+    return correctPlaceholders;
   }
-// The next object has rules for validation. That rules are functions which return boolean type.
-      var validationRules = {
-        'age': function(value, $currentValidationElement) {
-          var reg = /\d/g;
-          validateValue = value.replace(reg, '');
-          console.log(validateValue);
-          // console.log(typeof value);
-          // ar withNoDigits = questionText.replace(/[0-9]/g, '');
-          if (validateValue) {
-            console.log('wrong');
-            isFieldsValid = false;
-            highlightErrorField($currentValidationElement);
-            // $currentValidationElement.classList.add('errorField');
-          } else {
-            console.log('yes');
-            isFieldsValid = true;
-          }
-            return isFieldsValid;
-        },
-        'username': function(value) {
-          if (value.indexOf('user_') !== 0 ) {
-            console.log('wrong');
-            isFieldsValid = false;
-            highlightErrorField($currentValidationElement);
-          }
-          return isFieldsValid;
-        },
-        'date' : function(value) {
-          var today;
-          getCurrentDate();
 
-          if (today !== value) {
-             console.log('wrong');
-            isFieldsValid = false;
-            highlightErrorField($currentValidationElement);
-          }
+  function validation($currentForm, $elementsForValidation) {
+    $currentForm.addEventListener('submit', function (e) {
+      var isFormValid;
+      e.preventDefault();
 
-          function getCurrentDate() {
-          today = new Date();
-          var dd = today.getDate();
-          var mm = today.getMonth()+1;
-          var yyyy = today.getFullYear();
-          if(dd<10) {
-              dd='0'+dd
-          } 
-          if(mm<10) {
-              mm='0'+mm
-          } 
-          today = dd+'/'+mm+'/'+yyyy;
-          return today;
-        }
+      // We remove all error indicators  
+      removeErrorBorder($elementsForValidation);
+
+      // The nex function reset all red error borders from all fields 
+      function removeErrorBorder($elementsForValidation) {
+        for (var i = 0; i < $elementsForValidation.length; i++) {
+          $elementsForValidation[i].classList.remove('errorField');
+          $elementsForValidation[i].setAttribute('placeholder', correctPlaceholders[i]);
         }
       }
 
+      // Stat validation for each element
+      eachElementsValidation($elementsForValidation);
 
-        var isFieldsValid;
+      // If form valid we can submit it
+      if (isFormValid === true) {
+        $currentForm.submit();
+      }
+      
+      // Start functions describe
+
+      // The next function gets each field and applies validation for it. Also it has an  the object with all validation functions
+      function eachElementsValidation($elementsForValidation) {
+
+        // The next function create and set error indicators for each element
+        function highlightErrorField(errorElemet) {
+          errorElemet.classList.add('errorField');
+          errorElemet.value = '';
+          errorElemet.setAttribute('placeholder', 'Error! Empty/incorrectly filled');
+        }
+
+        // The next object has rules for validation. That rules are functions which return boolean type.
+        var validationRules = {
+          'age': function(value, $currentValidationElement) {
+            var reg = /\d/g;
+            var validateValue = value.replace(reg, '');
+            if (validateValue) {
+              isFieldsValid = false;
+              highlightErrorField($currentValidationElement);
+            } else {
+              isFieldsValid = true;
+            }
+            return isFieldsValid;
+          },
+          'username': function(value) {
+            if (value.indexOf('user_') !== 0 ) {
+              isFieldsValid = false;
+              highlightErrorField($currentValidationElement);
+            }
+            return isFieldsValid;
+          },
+          'date' : function(value) {
+            // We get current date and save it in a today variable
+            var today;
+            getCurrentDate();
+
+            if (today !== value) {
+             isFieldsValid = false;
+             highlightErrorField($currentValidationElement);
+            }
+
+            // The next function get current date
+            function getCurrentDate() {
+              today = new Date();
+              var dd = today.getDate();
+              var mm = today.getMonth() + 1;
+              var yyyy = today.getFullYear();
+              if(dd<10) {
+                dd = '0' + dd;
+              } 
+              if( mm < 10) {
+                mm = '0' + mm;
+              }
+
+              today = dd + '/' + mm + '/' + yyyy;
+              return today;
+            }
+          }
+        };
+
+      // End functions describe
+
+      // Tne next loop verifies user's value and return true or false in isFieldsValid variable 
+      var isFieldsValid;
       for (var i = 0; i < $elementsForValidation.length; i++) {
         if (isFieldsValid === false) {
           break;
         }
+
         var $currentValidationElement = $elementsForValidation[i];
         var currentValue = $currentValidationElement.value;
         // elementsForValidationName is a name of current elemet, which use for finding specific rule for that element
         var elementsForValidationName = $currentValidationElement.name;
         // validateFn is a function which implements specific rules for each element
-        var validateFn = validationRules[elementsForValidationName];
-        
+        var validateFn = validationRules[elementsForValidationName];  
+
         if ($currentValidationElement.value === '') {
           isFieldsValid = false;
           highlightErrorField($currentValidationElement);
           break; 
         }
+        // We use specific validate function for each field
         validateFn(currentValue, $currentValidationElement);
-    }
 
-    if (isFieldsValid === true) {
-      isFormValid = true;
-    } else {
-      isFormValid = false;
-    }
+        }
 
-    return isFormValid;
-}
-
-
-
-
-        
-
-
-
-
-
-
-
-
-
-
-
-
-
-   })
- }
+        if (isFieldsValid === true) {
+          isFormValid = true;
+        } else {
+          isFormValid = false;
+        }
+        return isFormValid;
+      }
+    });
+  }
 })();
